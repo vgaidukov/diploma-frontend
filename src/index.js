3d240e0d64ccb2e410508d85e9359e803d11174b
vgaidukov <vgaidukov@gmail.com>
2022-10-22 22:40:56 +0400

    set color: white for text

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './vendor/normalize.css';
 import './vendor/fonts/fonts.css';
+import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
