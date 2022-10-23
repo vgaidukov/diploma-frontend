ef86910961811264cd06c4ecfdb3e213a99f498a
vgaidukov <vgaidukov@gmail.com>
2022-10-23 19:23:48 +0400

    feat: header section on the main page layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,31 @@
-// import Promo from '../Promo/Promo'
+import '../Link/Link.css';
+import '../Header/Header.css';
+
+import { Switch, Route, Link } from 'react-router-dom';
+import headerLogo from '../../images/header-logo.svg';
 
 function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <section className='header'>
+            <Link className='link' to='/'>
+                <img className="header__logo" src={headerLogo} alt='логотип' />
+            </Link>
+            <div className='header__elements'>
+                <Switch>
+                    {/* <Route exact path='/login'>
+                        <Link className='header__link link' to='/register'>Регистрация</Link>
+                    </Route>
+                    <Route exact path='/register'>
+                        <Link className='header__link link' to='/login'>Вход</Link>
+                    </Route> */}
+                    <Route exact path='/'>
+                        <Link className='header__signup link' to='/login'>Регистрация</Link>
+                        <Link className='header__signin link' to='/login'>Войти</Link>
+                    </Route>
+
+                </Switch>
+            </div>
+        </section>
     )
 }
 
