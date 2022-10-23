d70a0fd624f5c09607e0e4dbaa531aa78b00149c
vgaidukov <vgaidukov@gmail.com>
2022-10-24 00:36:49 +0400

    feat: aboutMe layout

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,10 +1,25 @@
-// import Promo from '../Promo/Promo'
+import { Link } from 'react-router-dom';
+import './AboutMe.css'
+import studentPhoto from '../../images/student-photo.png'
 
 function AboutMe() {
     return (
-        <div>
-            AboutMe
-        </div>
+        <section className='about-me'>
+            <div className='about-me__header'>
+                <h2 className='about-me__title'>Студент</h2>
+                <div className='about-me__underline'></div>
+            </div>
+            <div className='student'>
+                <div className='student__info'>
+                    <h3 className='student__name'>Вадим</h3>
+                    <p className='student__specialization'>Фронтенд-разработчик, 34 года</p>
+                    <p className='student__description'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
+                        и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                    <Link className='link student__link'>Github</Link>
+                </div>
+                <img className='student__photo' src={studentPhoto} alt='Фото' />
+            </div>
+        </section>
     )
 }
 
