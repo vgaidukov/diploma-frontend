932c7860d857bf64266665aef2be120bc2ff2790
vgaidukov <vgaidukov@gmail.com>
2022-11-16 21:25:59 +0300

    add fullsize preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Preloader.css'
 
-const Preloader = ({ isLoading }) => {
+const Preloader = ({ isLoading, fullsize }) => {
     return (
-        <div className={`preloader ${isLoading && "preloader_visible"}`}>
+        <div className={`preloader ${isLoading && "preloader_visible"} ${fullsize && "preloader_fullsize"}`}>
             <div className="preloader__container">
                 <span className="preloader__round"></span>
             </div>
